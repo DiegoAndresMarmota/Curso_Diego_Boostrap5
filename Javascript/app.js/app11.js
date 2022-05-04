8d21fbd13ff9a1d2ff7a54415116fb4a0a13bbb3
Diego Echalar Penailillo <diegoechalarp@hotmail.com>
2022-05-03 20:41:26 -0400

    357.Practica Evento Click

---
+++ Javascript/app.js/app11.js
@@ -0,0 +1,13 @@
+// console.log("Validate");
+
+// console.log(document);
+// console.log(document.head)
+// console.log(document.title)
+// console.log(document.getElementById("tituloweb"))
+// console.log(document.getElementById("tituloweb").textContent);
+// console.log(document.getElementById("tituloweb").innerHTML);
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log(document.getElementById("tituloweb"));
+    console.log(document.getElementById("tituloweb").textContent);
+});
